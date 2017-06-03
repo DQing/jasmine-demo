4e7e56b2de17cfacef6decd542c03421082a7238
DQing <15829258737@163.com>
2017-06-03 18:12:30 +0800

    add jasmine test

---
+++ spec/PlayerSpec.js
@@ -0,0 +1,9 @@
+describe("Player", function () {
+    var index = require('../test');
+
+
+    it("should be able to play a Song", function () {
+        expect(index()).toBe('hello');
+    });
+
+});
